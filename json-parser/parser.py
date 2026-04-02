b89eac5a00165f2c0fc681e6c0827b62b95d8724
bhavya <bsg@somaiya.edu>
2026-04-02 10:13:37 -0700

    step2 tests pass with terrible solution

--- json-parser/parser.py
+++ json-parser/parser.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     parser.add_argument("file")
     arg = parser.parse_args()
 
-    # *** step1
+    # *** step2
 
     with open(arg.file, mode="r") as f: data = f.read().strip()
 
@@ -16,11 +16,22 @@ if __name__ == "__main__":
 
     stack = []
     for ch in data:
-        if ch == "}":
+        if ch.isspace(): continue
+        elif ch == "}":
             if not stack: sys.exit("invalid json: unmatched closing bracket")
-            stack.pop()
+            elif (stack[-1] != '{' and stack[-1] != '"'): sys.exit("invalid json: invalid or misplaced char")
+            else: stack.append(ch)
+        elif ch == '"':
+            if stack:
+                if stack[-1] in {'{', ':', ','}: stack.append(ch)
+                elif not stack[-1].isalnum(): sys.exit("invalid json: missing quote in some key")
+                else: stack.append(ch)
+        elif ch == ":" or ch == ",":
+            if stack and stack[-1] != '"': sys.exit("invalid json: missing quote in some value")
+            else: stack.append(ch)
         else: stack.append(ch)
-    if stack: sys.exit("invalid json: unmatched opening bracket")
+
+    if stack and stack[-1] != '}': sys.exit("invalid json: unmatched opening bracket")
 
     print("yay! valid json!")
     sys.exit(0)
